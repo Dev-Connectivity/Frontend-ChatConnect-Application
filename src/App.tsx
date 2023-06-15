556c84c5a5aafe34ba545297478c7f947e58278e
Shankhadeep Das <shankhadeepdas8@gmail.com>
2023-06-16 02:04:34 +0530

    boilerplate setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { LandingPage } from './landingPage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+        <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/landingPage/LandingPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <div>Hi, Welcome Back! 👋</div>
+  )
+}
